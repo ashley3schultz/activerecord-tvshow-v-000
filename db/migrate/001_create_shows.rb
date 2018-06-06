1599c4d7d99ce68b35cde8a9af87b4b16281b582
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-06 16:41:16 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -3,8 +3,8 @@ class CreateShows < ActiveRecord::Migration[5.1]
     create_table :shows do |t|
       t.string :name
       t.string :network
-      t.string :day 
+      t.string :day
       t.integer :rating
     end
   end
-end 
+end
