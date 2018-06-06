ef58bb2e029693d2d85a1cd08f47a030a8cf5a8e
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-06 17:11:08 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,30 +1,30 @@
 class Show < ActiveRecord::Base
 
-  def highest_rating
+  def self.highest_rating
     #returns the TV show with the highest rating
   end
 
-  def most_popular_show
+  def self.most_popular_show
     #returns the tv show with the highest rating
   end
 
-  def lowest_rating
+  def self.lowest_rating
     #returns the TV show with the lowest rating
   end
 
-  def least_popular_show
+  def self.least_popular_show
     #returns the tv show with the lowest rating
   end
 
-  def ratings_sum
+  def self.ratings_sum
     #returns the sum of all the ratings of all the tv shows
   end
 
-  def popular_shows
+  def self.popular_shows
     #returns an array of all of the shows with a rating above 5
   end
 
-  def shows_by_alphabetical_order
+  def self.shows_by_alphabetical_order
     #returns an array of all of the shows, listed in alphabetical order
   end
 
