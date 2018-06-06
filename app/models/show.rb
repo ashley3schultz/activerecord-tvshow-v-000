cea089915e05ba3430c8d5bc24248524817dc9c4
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-06 17:19:07 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -9,7 +9,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.lowest_rating
-    #returns the TV show with the lowest rating
+    self.order(rating: :asc).first
   end
 
   def self.least_popular_show
